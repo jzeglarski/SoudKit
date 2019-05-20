1823eda6da9a3b280f82d726b1b6e062628a16f9
JohnZeg <jzeglarski@gmail.com>
2019-05-20 18:06:33 -0400

    Initial Commit

---
+++ SoundKit/SoundKit.h
@@ -0,0 +1,19 @@
+//
+//  SoundKit.h
+//  SoundKit
+//
+//  Created by John Zeglarski on 5/20/19.
+//  Copyright © 2019 John Zeglarski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SoundKit.
+FOUNDATION_EXPORT double SoundKitVersionNumber;
+
+//! Project version string for SoundKit.
+FOUNDATION_EXPORT const unsigned char SoundKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoundKit/PublicHeader.h>
+
+
